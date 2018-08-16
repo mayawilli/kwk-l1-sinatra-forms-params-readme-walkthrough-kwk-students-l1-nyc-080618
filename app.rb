2b20a3dbdc36fe7b428454842d6040c7455d0749
mayawilli <nikeprosandbigbows23@gmail.com>
2018-08-16 17:51:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
+  get '/cheer_form' do
+    erb :cheer_form
   end
 
   # Add your post route and action below
+  
+  post '/cheer' do 
+    erb :cheer
+    
+  end
 
 end
